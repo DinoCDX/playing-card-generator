c03903d16722662f4ebb03d68f721f5594c8f6cf
DinoCDX <dinocdxreal@gmail.com>
2026-09-18 00:34:46 +0530

    Update generate-cards.js

--- playingcards/generate-cards.js
+++ playingcards/generate-cards.js
@@ -23,8 +23,8 @@ async function screenshotCard(page, html, filePath) {
 
 (async () => {
   const browser = await puppeteer.launch({
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-});
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setViewport(CARD_PX);
 
